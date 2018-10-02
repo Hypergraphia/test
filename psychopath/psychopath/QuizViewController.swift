392521d23cfc4a69ec59e2b835cf3592f8864d2e
Hypergraphia <42059203+Hypergraphia@users.noreply.github.com>
2018-10-02 18:54:41 +0900

    Add files via upload

---
+++ psychopath/psychopath/QuizViewController.swift
@@ -0,0 +1,96 @@
+//
+//  QuizViewController.swift
+//  psychopath
+//
+//  Created by 新井達朗 on 2018/10/01.
+//  Copyright © 2018年 新井達朗. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    
+    let question = ["家に強盗がやってきた。あなたは武器を持っておらず、隠れる事しかできない。あなたが身を隠すとしたら家のどこ？",
+                    "あなたを影から見ているヒトがいる。そのヒトは何歳？",
+                    "あなたは囚人だったが、刑期を終え、社会に出た。まず何をする？",
+                    "あなたの前に戦時中で怪我をした軍人の肖像画がある。どこに怪我をしている？",
+                    "あなたと友人は無人島で遭難した。持ち物はパン一切れとサバイバルナイフ。周りには木と石のみ。食料をどうする？",
+                    "あなたは一面木で囲まれた深い山奥にいる。目の前にはちょっと休憩スペースがあるが、その後ろを何かがざっと過ぎ去った。果たしてそれは何か？",
+                    "あなたには恋人がいる。しかし、その人は大量殺人鬼だった。『殺される前に殺す』か、『殺されるまで待つ』しか選択肢がないものとする。あなたはどちらを選ぶ？",
+                    "あなたはAさんに恋をしました。しかしAさんにはBという恋人が居ました。あなたはどうしますか？",
+                    "世界から色が消えて白黒の世界になりました。一色だけ好きな色を加えれるとしたら何色？"]
+    let answers = [["ドアの裏","押し入れ","物置き"], ["同い年","年下","年上"], ["家を探す","仕事を探す","家族に会う"], ["胸","足","頭"], ["友人にあげる","独り占めする","半分に分ける"], ["人","動物","落ち葉"], ["殺される前に殺す","殺されるまで待つ","選べない"], ["Aさんを殺す","Bさんを殺す","自殺する"], ["赤色","青色","黄色"]]
+    
+    //variabless
+    var currentQuestion = 0
+    var rightAnswerPlacement:UInt32 = 0
+    var points = 0;
+    
+    //textView
+    @IBOutlet weak var textview: UITextView!
+    
+    //button
+    @IBAction func action(_ sender: AnyObject)
+    {
+        if (sender.tag == Int(rightAnswerPlacement)) {
+            print("RIGHT!")
+            points += 1
+        } else {
+            print("WRONG!!!")
+        }
+        
+        if (currentQuestion != question.count) {
+            newQuestion()
+        } else {
+            performSegue(withIdentifier: "showScore", sender: self.points)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        newQuestion()
+    }
+    
+    //Function that displays new Question
+    func newQuestion() {
+        textview.text = question[currentQuestion]
+        
+        rightAnswerPlacement = arc4random_uniform(3)+1
+        
+        //Create a button
+        var button:UIButton = UIButton()
+        
+        var x = 1
+        
+        for i in 1...3 {
+            //create a button
+            button = view.viewWithTag(i) as! UIButton
+            
+            if (i == Int(rightAnswerPlacement)) {
+                button.setTitle(answers[currentQuestion][0], for: .normal)
+            } else {
+                button.setTitle(answers[currentQuestion][x], for: .normal)
+                x = 2
+            }
+        }
+        currentQuestion += 1
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ psychopath/psychopath/ResultViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ResultViewController.swift
+//  psychopath
+//
+//  Created by 新井達朗 on 2018/10/01.
+//  Copyright © 2018年 新井達朗. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    let points = Int.random(in:1...10)
+    
+    @IBOutlet weak var result: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        switch points {
+        case 1:
+            result.text = "10%です"
+        case 2:
+            result.text = "20%です"
+        case 3:
+            result.text = "30%です"
+        case 4:
+            result.text = "40%です"
+        case 5:
+            result.text = "50%です"
+        case 6:
+            result.text = "60%です"
+        case 7:
+            result.text = "70%です"
+        case 8:
+            result.text = "80%です"
+        case 9:
+            result.text = "90%です"
+        default:
+            result.text = "0%です"
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
